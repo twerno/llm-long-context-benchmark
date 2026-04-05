6fcd47dc3fa7e5963e24fa10eb8d584ded740084
twerno <twerno@gmail.com>
2026-04-05 10:49:23 +0200

    HiddenPhraseTest

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { LMStudioApiRunner } from "./llmRunner/APILLMRunner";
+import HiddenTextTest from "./test/HiddenPhraseTest";
 import SequenceOfNumbersTest from "./test/SequenceOfNumbersTest"
 
 async function test() {
@@ -6,6 +7,13 @@ async function test() {
   // let resp = await LMStudioApiRunner.run({ prompt: ["Poland is the capital city of which country?"] })
   // console.log(resp);
 
+  await testHiddenText();
+
+}
+
+test()
+
+async function testSequenceOfNumbers() {
   const testPrompt = await SequenceOfNumbersTest.buildPrompt({ sequenceLength: 1820, noOfGaps: 2 })
   // console.log(testPrompt.prompt)
   let resp = await LMStudioApiRunner.run({ prompt: [testPrompt.prompt] })
@@ -15,4 +23,12 @@ async function test() {
   console.log(`Result: correct [${evaluate.correct}/${evaluate.total}] false possitive: ${evaluate.falsePossitive}`)
 }
 
-test()
+async function testHiddenText() {
+  const testPrompt = await HiddenTextTest.buildPrompt({ textLength: 100000, noOfHiddenPhrases: 200 })
+  console.log(testPrompt.prompt)
+  let resp = await LMStudioApiRunner.run({ prompt: [testPrompt.prompt] })
+  const evaluate = await HiddenTextTest.evaluate(resp.output, testPrompt.data)
+  console.log(testPrompt.data.hiddenPhrases)
+  console.log(resp)
+  console.log(`Result: correct [${evaluate.correct}/${evaluate.total}] false possitive: ${evaluate.falsePossitive}`)
+}
